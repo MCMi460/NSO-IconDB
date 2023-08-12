335729fe399599f78d96cd59fcbf78b72b33527f
Deltaion Lee <32529306+MCMi460@users.noreply.github.com>
2023-08-12 15:33:35 -0500

    Add button scrolling to embed

--- server/bot_extension.py
+++ server/bot_extension.py
@@ -11,6 +11,86 @@ class NSO_IconDB(commands.Cog):
         self.icons = []
         self.url = 'https://icondb.nsorpc.com'
 
+    def embed_constructor(self, n = 0):
+        embed = discord.Embed(
+            title = 'Nintendo Switch Online Icons Database',
+            url = 'https://icondb.nsorpc.com/',
+            description = 'Current Icons on the NSO Applet',
+            color = 0xe60012,
+        )
+        embed.set_author(
+            name = 'MCMi460',
+            url = 'https://github.com/MCMi460',
+            icon_url = 'https://avatars.githubusercontent.com/u/32529306',
+        )
+        for icon in self.icons[n * 25:(n + 1) * 25]:
+            embed.add_field(
+                name = '%s' % icon[1],
+                value = '[%s Icon](%s%s)' % (icon[0], self.url, icon[2]),
+                inline = True,
+            )
+        embed.set_footer(
+            text = 'Provided by nso-applet-api',
+        )
+        return embed
+
+    class Buttons(discord.ui.View):
+        def __init__(self, *,
+            timeout = 180,
+            icons = [],
+            index = 0,
+            embed_constructor = None,
+        ):
+            super().__init__(timeout = timeout)
+            self.icons = icons
+            self.index = index
+            self.embed_constructor = embed_constructor
+
+            self.check(0)
+
+        def check(self, n):
+            if self.icons > (n + 1) * 25:
+                self.children[1].disabled = False
+                self.children[1].style = discord.ButtonStyle.blurple
+            else:
+                self.children[1].disabled = True
+                self.children[1].style = discord.ButtonStyle.red
+
+            if n > 0:
+                self.children[0].disabled = False
+                self.children[0].style = discord.ButtonStyle.blurple
+            else:
+                self.children[0].disabled = True
+                self.children[0].style = discord.ButtonStyle.red
+
+        @discord.ui.button(
+            label = '⬅️',
+            style = discord.ButtonStyle.red,
+            disabled = True,
+        )
+        async def left_button(self, interaction:discord.Interaction, button:discord.ui.Button):
+            self.index -= 1
+            embed = self.embed_constructor(self.index)
+            self.check(self.index)
+            await interaction.response.edit_message(
+                embed = embed,
+                view = self,
+            )
+
+        @discord.ui.button(
+            label = '➡️',
+            style = discord.ButtonStyle.red,
+            disabled = True,
+        )
+        async def right_button(self, interaction:discord.Interaction, button:discord.ui.Button):
+            self.index += 1
+            embed = self.embed_constructor(self.index)
+            self.check(self.index)
+            await interaction.response.edit_message(
+                embed = embed,
+                view = self,
+            )
+
     @commands.Cog.listener()
     async def on_ready(self):
         print('[NSO-IconDB ready]')
@@ -27,37 +107,24 @@ class NSO_IconDB(commands.Cog):
         os.chdir('../../')
 
     @discord.app_commands.command()
+    @discord.app_commands.checks.cooldown(1, 10) # Try to prevent spamming of this command
     async def icons(self, interaction: discord.Interaction):
         '''
         Shows the current Nintendo Switch Online icons!
         '''
-        embed = discord.Embed(
-            title = 'Nintendo Switch Online Icons Database',
-            url = 'https://icondb.nsorpc.com/',
-            description = 'Current Icons on the NSO Applet',
-            color = 0xe60012,
+        embed = self.embed_constructor(0)
+        await interaction.response.send_message(
+            embed = embed,
+            view = self.Buttons(
+                icons = len(self.icons),
+                embed_constructor = self.embed_constructor,
+            ),
         )
-        embed.set_author(
-            name = 'MCMi460',
-            url = 'https://github.com/MCMi460',
-            icon_url = 'https://avatars.githubusercontent.com/u/32529306',
-        )
-        for icon in self.icons[:24]:
-            embed.add_field(
-                name = '%s' % icon[1],
-                value = '[%s Icon](%s%s)' % (icon[0], self.url, icon[2]),
-                inline = True,
-            )
-        if len(self.icons) >= 25:
-            embed.add_field(
-                name = 'And more!',
-                value = '[See more here](%s)' % self.url,
-                inline = True,
-            )
-        embed.set_footer(
-            text = 'Provided by nso-applet-api',
-        )
-        await interaction.response.send_message(embed = embed)
+
+    @icons.error
+    async def icons_error(self, interaction: discord.Interaction, error: discord.app_commands.AppCommandError):
+        if isinstance(error, discord.app_commands.CommandOnCooldown):
+            await interaction.response.send_message(str(error), ephemeral = True)
 
 async def setup(bot):
     await bot.add_cog(NSO_IconDB(bot))
